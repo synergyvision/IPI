9e859b7a8ccf5c2fbfe38d22821f14f8042f539b
Danny Morales <dannymorales@MacBook-Pro-de-Danny.local>
2019-08-05 12:53:06 -0400

    VisionInsurance® Insurance Performance Indicators

---
+++ app/global.r
@@ -0,0 +1,28 @@
+rm(list = ls())
+
+# Make sure a package is at least some version (only installs from CRAN)
+
+
+
+#cargo librerias a usar
+
+
+# Encabezado Vision
+VisionHeader <- function(){tags$head(
+  tags$link(rel = "stylesheet", type = "text/css", href = "app.css"),
+  tags$img(src="img/vision1.png" , id = "VisionLogo", width = 130 ),
+  singleton(includeScript("www/js/d3.js")),
+  singleton(includeScript("www/js/underscore.js")),
+  singleton(includeScript("www/js/jquery-ui.js")),
+  singleton(includeCSS("www/css/app.css"))
+)}
+
+ACERTITLE_TEXT<-"Acerca de VisionRisk™"
+ACERSUBSV_TEXT<-"Tecnología para Especulación, Inversión, Economía, Finanzas y Riesgo"
+ACERVER_TEXT<-"Versión: 0.0.0"
+ACERRIF_TEXT<-"Rif: "
+ACERRS_TEXT<-"Copyright © 2014-2018 Synergy Vision"
+ACERRS_TEXT2 <- "All Rights Reserved"
+ACERDIR_TEXT<-"Centro Gerencial Mohedano, La Castellana"
+ACERTLF_TEXT<-"0212-2630808 / 0414-2769752"
+ACERCORR_TEXT<-"contacto@synergy.vision"
